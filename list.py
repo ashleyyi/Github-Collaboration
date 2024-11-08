2a03384b2ac2182c7153697e71c27c0d1a218a79
ashleyyi <ashley02.yi@gmail.com>
2024-11-07 20:02:19 -0500

    new additions

--- list.py
+++ list.py
@@ -4,3 +4,13 @@ def get_even_numbers(numbers):
 # Example usage:
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(get_even_numbers(numbers))  # Output: [2, 4, 6, 8, 10]
+
+count = 0
+
+newList = []
+
+while count < 10:
+    if numbers[count] % 2 != 0:
+        newList.append(numbers[count])
+
+print(newList)
