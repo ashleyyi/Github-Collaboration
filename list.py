d9b6bb8d17e77f28013c8cd4985784b5b17b2fb3
vi-kinn <vishalkinner9814@gmail.com>
2024-11-07 19:58:48 -0500

    made chnages

--- list.py
+++ list.py
@@ -6,8 +6,8 @@ def get_even_numbers(numbers):
 def get_odd_numbers(numbers):
     return [num for num in numbers if num % 2 != 0]
 
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(get_odd_numbers(numbers)) 
 # Example usage:
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 print(get_even_numbers(numbers)) 
